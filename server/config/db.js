7ea31462856156a71c5b097e48db08f6a44b3cb9
hassaanbk <90360809+hassaanbk@users.noreply.github.com>
2021-11-02 23:19:19 -0400

    thid commit

---
+++ server/config/db.js
@@ -0,0 +1,4 @@
+module.exports = {
+  //local MongoDB deployment ->
+  "URI": "mongodb+srv://hassaan:w6uDqI2gjxRJYyly@portfolio-cluster.3agyo.mongodb.net/portfolio-cluster?retryWrites=true&w=majority"
+};
